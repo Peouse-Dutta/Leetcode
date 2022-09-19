e8e74711b3170814f523447b9c1ab4401e95e657
Peouse-Dutta <pdrax26@gmail.com>
2022-09-20 00:02:55 +0530

    String to number

---
+++ 13-RomanToInt.js
@@ -0,0 +1,54 @@
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+const romanToInt = (s) => {
+    const data = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    switch (s) {
+        case "IV":
+            s = s.replace("IV", "IIII");
+            console.log(s);
+            break;
+        case "IX":
+            s = s.replace("IX", "VIIII");
+            console.log(s);
+            break;
+        case "XL":
+            s = s.replace("XL", "XXXX");
+            console.log(s);
+            break;
+        case "XC":
+            s = s.replace("XC", "CXXXX");
+            console.log(s);
+            break;
+        case "CD":
+            s = s.replace("CD", "CCCC");
+            console.log(s);
+            break;
+        case "CM":
+            s = s.replace("CM", "DCCCC");
+            console.log(s);
+            break;
+    }
+    let val = 0;
+    Array.from(s.split("")).forEach((e)=>{
+        val = val + data.e;
+        console.log(e);
+    })
+    return val;
+};
+
+
+console.log(romanToInt("IV"));
